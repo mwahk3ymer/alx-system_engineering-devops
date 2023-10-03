9aa0f0c45bfd1d29ea9f01d9494c2b0fbb78f11b
Mwahk3ymer <audreymwakima99@gmail.com>
2023-10-03 02:09:05 -0700

    create a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there is exactly one argument provided
-if ARGV.length != 1
-  puts "Usage: ruby script.rb <string>"
-  exit 1
-end
-
-# Define the regular expression using Oniguruma syntax
-regex = /School/
-
-# Get the input argument
-input_string = ARGV[0]
-
-# Check if the input string matches the regex
-if regex.match(input_string)
-  puts "Match found: #{input_string}"
-else
-  puts "No match found for 'School' in #{input_string}"
-end
+puts ARGV[0].scan(/School/).join
