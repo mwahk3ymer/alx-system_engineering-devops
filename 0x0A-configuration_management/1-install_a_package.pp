3b2379db18cb5f973a7f7d0d8743b7758812a3f3
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-24 22:10:11 +0000

    pip n killl

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3.
+# install puppet-lint -v 2.5.0
 
-exec { 'flask':
-  command  => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
-  provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
-# This Puppet manifest kills the process named "killmenow"
+# kill process killmenow
 
-exec { 'killmenow_process':
-  command     => 'pkill killmenow',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
-  subscribe   => File['/path/to/your/killmenow_script'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
