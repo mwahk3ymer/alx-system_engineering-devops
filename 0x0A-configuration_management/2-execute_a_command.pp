999647479b1493d570cab04d531208e24aa1f452
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-24 22:03:51 +0000

    kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -6,9 +6,3 @@ exec { 'killmenow_process':
   refreshonly => true,
   subscribe   => File['/path/to/your/killmenow_script'],
 }
-
-file { '/path/to/your/killmenow_script':
-  ensure  => present,
-  mode    => '0755',
-  content => "#!/bin/bash\nwhile [[ true ]]; do sleep 2; done\n",
-}
