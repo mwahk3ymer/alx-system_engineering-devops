52280d8022e07e47b4027fff24f70dfe37f01e36
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-24 22:07:29 +0000

    kill command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # This Puppet manifest installs Flask version 2.1.0 using pip3.
 
-package { 'flask':
-  ensure   => '2.1.0',
+exec { 'flask':
+  command  => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
   provider => 'pip3',
 }
