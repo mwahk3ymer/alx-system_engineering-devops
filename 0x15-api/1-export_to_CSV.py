61455420471281ecea6e0766f44c50847cb22309
mwahk3ymer <audreymwakima99@gmail.com>
2024-01-03 11:53:09 +0000

    csv2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Returns to-do list information for data in CSV format."""
-import requests
+"""Returns to-do list information for a given employee ID and exports data in CSV format."""
 import csv
+import requests
 import sys
-
 
 def export_to_csv(user_id, user_name, tasks):
     """Export tasks to a CSV file."""
@@ -24,19 +23,15 @@ def export_to_csv(user_id, user_name, tasks):
 
     print(f"Data exported to {filename}")
 
-
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    completed_tasks = [t for t in todos if t.get("completed")]
+    print(f"Employee {user['name']} is done with tasks({len(todos)}/{len(todos)}):")
 
-    print(f"Employee {user.get('name')} is done ({len(completed_tasks)}/"
-          f"{len(todos)}):")
+    for task in todos:
+        print(f"\t{task['title']}")
 
-    for task in completed_tasks:
-        print(f"\t{task.get('title')}")
-
-    export_to_csv(user_id, user.get("name"), completed_tasks)
+    export_to_csv(user_id, user['name'], todos)
