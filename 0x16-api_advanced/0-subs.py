2ed77cc5347bb2a8487ee57a57586b421faea969
mwahk3ymer <audreymwakima99@gmail.com>
2024-01-16 09:22:00 +0000

    asubpy

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,10 +16,9 @@ def number_of_subscribers(subreddit):
         int: Number of subscribers if the subreddit is valid, 0 otherwise.
     """
     try:
-        # Set a custom User-Agent to avoid errors related to Too Many Requests
+
         headers = {'User-Agent': 'Custom User Agent'}
         
-        # Make a GET request to the Reddit API for the subreddit information
         response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers)
 
         # Check if the request was successful (status code 200)
@@ -29,20 +28,8 @@ def number_of_subscribers(subreddit):
             subscribers = data['data']['subscribers']
             return subscribers
         else:
-            print(f"Error: {response.status_code}")
+          #  print(f"0")
             return 0
     except Exception as e:
         print(f"An error occurred: {e}")
         return 0
-
-# Example usage
-if __name__ == "__main__":
-    # Replace 'python' with the subreddit you want to query
-    subreddit_name = 'python'
-    
-    # Call the function with the subreddit name
-    subscribers_count = number_of_subscribers(subreddit_name)
-    
-    # Print the result
-    print(f"The subreddit '{subreddit_name}' has {subscribers_count} subscribers.")
-
